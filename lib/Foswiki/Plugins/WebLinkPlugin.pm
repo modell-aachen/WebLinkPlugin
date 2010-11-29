34ef1fa409c037d1d356a7e1fe82501edb66ed9e
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-11-29 15:26:14 +0000

    Item10092: externalized %WEBLINK from NatSkinPlugin
    git-svn-id: http://svn.foswiki.org/trunk/WebLinkPlugin@10104 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

---
+++ lib/Foswiki/Plugins/WebLinkPlugin.pm
@@ -0,0 +1,81 @@
+# Plugin for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+#
+# WebLinkPlugin is Copyright (C) 2010 Michael Daum http://michaeldaumconsulting.com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details, published at
+# http://www.gnu.org/copyleft/gpl.html
+
+package Foswiki::Plugins::WebLinkPlugin;
+
+use strict;
+use warnings;
+
+=begin TML
+
+---+ package WebLinkPlugin
+
+=cut
+
+use Foswiki::Func ();
+
+our $VERSION = '$Rev$';
+our $RELEASE = '1.00';
+our $SHORTDESCRIPTION = 'A parametrized %WEB macro';
+our $NO_PREFS_IN_TOPIC = 1;
+our $baseWeb;
+our $baseTopic;
+our $doneInit;
+
+
+=begin TML
+
+---++ initPlugin($topic, $web, $user) -> $boolean
+
+=cut
+
+sub initPlugin {
+  ($baseTopic, $baseWeb) = @_;
+
+  Foswiki::Func::registerTagHandler('WEBLINK', \&WEBLINK);
+
+  $doneInit = 0;
+  return 1;
+}
+
+=begin TML
+
+---++ init()
+
+=cut
+
+sub init {
+  return if $doneInit;
+
+  $doneInit = 1;
+
+  require Foswiki::Plugins::WebLinkPlugin::Core;
+  Foswiki::Plugins::WebLinkPlugin::Core::init($baseWeb, $baseTopic);
+}
+
+=begin TML
+
+---+ WEBLINK($session, $params, $theTopic, $theWeb) -> $string
+
+stub for WEBLINK to initiate the core before handling the macro
+
+=cut
+
+sub WEBLINK {
+  init();
+  return Foswiki::Plugins::WebLinkPlugin::Core::WEBLINK(@_);
+}
+
+1;
---
+++ lib/Foswiki/Plugins/WebLinkPlugin/Core.pm
@@ -0,0 +1,130 @@
+# Plugin for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+#
+# WebLinkPlugin is Copyright (C) 2010 Michael Daum http://michaeldaumconsulting.com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details, published at
+# http://www.gnu.org/copyleft/gpl.html
+
+package Foswiki::Plugins::WebLinkPlugin::Core;
+
+use strict;
+use warnings;
+
+=begin TML
+
+---+ package WebLinkPlugin::Core
+
+=cut
+
+use Foswiki::Func ();
+
+our $baseWeb;
+our $baseTopic;
+
+use constant DEBUG => 0; # toggle me
+
+=begin TML
+
+---++ writeDebug($message(
+
+prints a debug message to STDERR when this module is in DEBUG mode
+
+=cut
+
+sub writeDebug {
+  print STDERR "WebLinkPlugin::Core - $_[0]\n" if DEBUG;
+}
+
+=begin TML
+
+---++ init($baseWeb, $baseTopic)
+
+initializer for the plugin core; called before any macro hanlder is executed
+
+=cut
+
+sub init {
+  ($baseWeb, $baseTopic) = @_;
+}
+
+=begin TML
+
+---++ WEBLINK($session, $params, $theTopic, $theWeb) -> $result
+
+implementation of this macro
+
+=cut
+
+sub WEBLINK {
+  my ($session, $params, $topic, $web) = @_;
+
+  writeDebug("called WEBLINK()");
+
+  # get params
+  my $theWeb = $params->{_DEFAULT} || $params->{web} || $web;
+  my $theName = $params->{name};
+  my $theMarker = $params->{marker} || 'current';
+  my $theClass = $params->{class} || 'webLink';
+
+  my $defaultFormat =
+    '<a class="'.$theClass.' $marker" href="$url" title="%ENCODE{"$tooltip" type="html"}%">$title</a>';
+
+  my $theFormat = $params->{format} || $defaultFormat;
+
+  #writeDebug("theFormat=$theFormat, theWeb=$theWeb");
+
+  my $theTooltip = $params->{tooltip} ||
+    Foswiki::Func::getPreferencesValue('WEBSUMMARY', $theWeb) || 
+    Foswiki::Func::getPreferencesValue('SITEMAPUSETO', $theWeb) || '';
+
+  my $homeTopic = Foswiki::Func::getPreferencesValue('HOMETOPIC') 
+    || $Foswiki::cfg{HomeTopicName} 
+    || 'WebHome';
+
+  my $theUrl = $params->{url} ||
+    $session->getScriptUrl(0, 'view', $theWeb, $homeTopic);
+
+  # unset the marker if this is not the current web 
+  $theMarker = '' unless $theWeb eq $baseWeb;
+
+  # normalize web name
+  $theWeb =~ s/\//\./go;
+
+  # get a good default name
+  unless ($theName) {
+    $theName = $theWeb;
+    $theName = $2 if $theName =~ /^(.*)[\.](.*?)$/;
+  }
+
+  my $title = '';
+  if ($theFormat =~ /\$title/) {
+    if (Foswiki::Func::getContext()->{DBCachePluginEnabled}) {
+      require Foswiki::Plugins::DBCachePlugin;
+      $title = Foswiki::Plugins::DBCachePlugin::getTopicTitle($theWeb, $homeTopic);
+    }
+    $title = $theName if $title eq $homeTopic;
+  }
+
+  my $result = $theFormat;
+  $result =~ s/\$default/$defaultFormat/g;
+  $result =~ s/\$marker/$theMarker/g;
+  $result =~ s/\$url/$theUrl/g;
+  $result =~ s/\$tooltip/$theTooltip/g;
+  $result =~ s/\$name/$theName/g;
+  $result =~ s/\$title/$title/g;
+  $result =~ s/\$web/$theWeb/g;
+  $result =~ s/\$topic/$homeTopic/g;
+
+  #writeDebug("result=$result");
+  return $result;
+}
+
+1;
